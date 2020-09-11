b869593ce651ac08c4a6d664f4e6ec55bd2c1d00
LaneBlasingame <lgblasingame1@buffs.wtamu.edu>
2020-09-10 20:46:38 -0500

    the working version

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -16,27 +16,26 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
-        // private readonly ILogger<HomeController> _logger;
 
-        // public HomeController(ILogger<HomeController> logger)
-        // {
-        //     _logger = logger;
-        // }
+        
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
 
-        // public IActionResult Index()
-        // {
-        //     return View();
-        // }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
 
-        // public IActionResult Privacy()
-        // {
-        //     return View();
-        // }
-
-        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+            
+        }
     }
 }
