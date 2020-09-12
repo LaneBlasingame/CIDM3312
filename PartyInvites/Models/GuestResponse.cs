bb85e557ceacba166a18c705c47f2f8547133986
LaneBlasingame <lgblasingame1@buffs.wtamu.edu>
2020-09-12 13:41:04 -0500

    the close version

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -4,13 +4,20 @@ namespace PartyInvites.Models {
 
     public class GuestResponse {
 
+
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
 
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
 
-        public bool WillAttend { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
 
     }
 }
